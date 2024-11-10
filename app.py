3e391771d9599561cea5bbd2cef1bf99761379bf
unknown <aditiravi04@gmail.com>
2024-11-10 04:59:29 -0500

    final front/back combine

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template, request
+import os
+from flask import Flask, jsonify, render_template, request, send_from_directory
 import pickle
 import numpy as np
 import scraping as s 
@@ -6,6 +7,25 @@ import scraping as s
 app = Flask(__name__)
 # model = pickle.load(open('model.pkl', 'rb'))
 
+@app.route('/navBar.html')
+def serve_navbar():
+    return send_from_directory(os.path.join(app.root_path), 'navBar.html')
+
+@app.route('/SearchBar.html')
+def serve_searchbar():
+    return send_from_directory(os.path.join(app.root_path), 'SearchBar.html')
+
+@app.route('/sbStyle.css')
+def serve_sbStyle():
+    return send_from_directory(os.path.join(app.root_path), 'sbStyle.css')
+
+@app.route('/downloaded_images/logo2.png')
+def serve_image():
+    return send_from_directory(os.path.join(app.root_path, 'downloaded_images'), 'logo2.png')
+
+@app.route('/index.html')
+def index_html():
+    return send_from_directory(os.path.join(app.root_path), 'index.html')
 
 @app.route('/get-products', methods=['GET'])
 def get_products():
@@ -14,8 +34,10 @@ def get_products():
         return "no query provided"
     print("URL received: ", query)
     ingredients_list = s.get_ingredients(query)
-    print(ingredients_list)
-    return render_template('temp.html', results=ingredients_list)  
+    #print(ingredients_list)
+    arr = [["glossier", ingredients_list], ["glossier", ingredients_list], ["glossier", ingredients_list], ["glossier", ingredients_list], ["glossier", ingredients_list]]
+    print(arr)
+    return render_template('test.html', results=arr)  
    
 
 if __name__ == "__main__":
